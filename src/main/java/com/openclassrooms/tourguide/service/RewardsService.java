c35e67fc8576c03cf7be7e57ffd0243af3d5cab6
David <davidroussel171113@outlook.fr>
2025-09-20 21:38:04 +0200

    Remise en etat du calcul

--- src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
+++ src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
@@ -48,20 +48,13 @@ public class RewardsService {
 		CopyOnWriteArrayList<Attraction> attractions1 = new CopyOnWriteArrayList<>(attractions);
 
 			 for (VisitedLocation location : locations){
-				 CompletableFuture.runAsync(() -> {
 					for (Attraction attraction : attractions1) {
-						if (user.getUserRewards().stream().noneMatch(userReward -> userReward.attraction.attractionName.equals(attraction.attractionName))) {
-
-
+						if (user.getUserRewards().stream().noneMatch(r -> r.attraction.attractionName.equals(attraction.attractionName))) {
 								if (nearAttraction(location, attraction)) {
 									user.addUserReward(new UserReward(location, attraction, getRewardPoints(attraction, user)));
 								}
-							//Noter tester de crée un ComptableFuture<UserReward> avec un supplyAsync afin de crée un tableau pour stocker tout les UserReward et ainsi les retourner
-							//a voir si changement de l'appel de la méthode de void à UserReward.
-
 						}
 					}
-				 });
 			}
 	}
 	
