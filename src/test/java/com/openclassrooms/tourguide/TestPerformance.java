0968011d59867e001141b7b55c4e58958e84c641
David <davidroussel171113@outlook.fr>
2025-10-07 21:50:26 +0200

    Ajout de la nouvelle méthode trackUserLocation(List<User> users) pour validation test passage 100000 users

--- src/test/java/com/openclassrooms/tourguide/TestPerformance.java
+++ src/test/java/com/openclassrooms/tourguide/TestPerformance.java
@@ -51,16 +51,14 @@ public class TestPerformance {
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		// Users should be incremented up to 100,000, and test finishes within 15
 		// minutes
-		InternalTestHelper.setInternalUserNumber(100);
+		InternalTestHelper.setInternalUserNumber(100000);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 
 		List<User> allUsers = tourGuideService.getAllUsers();
 
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
-		for (User user : allUsers) {
-			tourGuideService.trackUserLocation(user);
-		}
+		tourGuideService.trackUserLocation(allUsers);
 		stopWatch.stop();
 		tourGuideService.tracker.stopTracking();
 
